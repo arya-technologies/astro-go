27483864c12c06af4d59b87699f1df11e098bdc7
MRINMOY MONDAL <mm2922894@gmail.com>
2024-08-18 01:13:56 +0530

    up

--- src/app/settings/index.tsx
+++ src/app/settings/index.tsx
@@ -1,10 +1,12 @@
 import ScrollView from "@/components/ScrollView";
 import React from "react";
 import AppearanceSettings from "@/components/settings/AppearanceSettings";
+import CameraSettings from "@/components/settings/CameraSettings";
 
 export default function Appearance() {
   return (
     <ScrollView>
+      <CameraSettings />
       <AppearanceSettings />
     </ScrollView>
   );
