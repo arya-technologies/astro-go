62e6310c0cc63fb8d648a5ec98d6bebfd39164aa
MRINMOY MONDAL <mm2922894@gmail.com>
2024-08-17 20:37:14 +0530

    applied reduced motion to never

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,6 +5,7 @@ import * as NavigationBar from "expo-navigation-bar";
 import { Stack } from "expo-router/stack";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { ActivityIndicator } from "react-native-paper";
+import { ReducedMotionConfig, ReduceMotion } from "react-native-reanimated";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
@@ -21,6 +22,7 @@ export default function Layout() {
           }}
         >
           <GestureHandlerRootView style={{ flex: 1 }}>
+            <ReducedMotionConfig mode={ReduceMotion.Never} />
             <Stack
               screenOptions={{
                 animation: "default",
